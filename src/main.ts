51314b7c271950333c8d709a38147a4fc53922b2
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-25 23:51:10 -0500

    posixifyPathNormalized

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ export function posixifyPath(givenPath: string): string {
   return normalize(givenPath).replace(/\\/g, "/")
 }
 
+/** Converts given path to Posix (replacing \\ with /) and removing ending slashes
+ * @param {string} givenPath   Path to convert
+ * @returns {string}          Converted filepath
+ */
+export function posixifyPathNormalized(givenPath: string): string {
+  return posixifyPath(givenPath).replace(/\/$/, "")
+}
