14e40015d8fd997db4512ce938d26bce8feefc3a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-25 23:51:27 -0500

    getPathType

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { join, normalize } from "path"
+import { promises } from "fs"
+const { readFile, stat } = promises
+import isPath from "is-valid-path"
+import { unique } from "./utils"
 
 /** Converts given path to Posix (replacing \\ with /)
  * @param {string} givenPath   Path to convert
@@ -14,4 +18,29 @@ export function posixifyPath(givenPath: string): string {
  */
 export function posixifyPathNormalized(givenPath: string): string {
   return posixifyPath(givenPath).replace(/\/$/, "")
+}
+/** Enum that specifies the path type. 1 for directory, 2 for file, 0 for others */
+enum PATH_TYPE {
+  OTHER,
+  DIRECTORY,
+  FILE,
+}
+
+/** Get the type of the given path
+ * @param {string} givenPath absolute path
+ * @returns {Promise<PATH_TYPE>}
+ */
+async function getPathType(filepath: string): Promise<PATH_TYPE> {
+  let pathStat
+  try {
+    pathStat = await stat(filepath)
+  } catch (error) {
+    return PATH_TYPE.OTHER
+  }
+  if (pathStat.isDirectory()) {
+    return PATH_TYPE.DIRECTORY
+  } else if (pathStat.isFile()) {
+    return PATH_TYPE.FILE
+  }
+  return PATH_TYPE.OTHER
 }
